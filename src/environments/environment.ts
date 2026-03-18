ad1306699d6480d89157110dc1a9a064166fdedc
Joana Freitas <Joanacf1005@gmail.com>
2026-03-18 11:28:17 +0000

    FIX DEPLOY

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,5 @@
-export const environment = {};
+export const environment = {
+  production: true,
+  supabaseUrl: process.env['SUPABASE_URL'] || 'SUPABASE_URL',
+  supabaseKey: process.env['SUPABASE_KEY'] || 'SUPABASE_ANON_KEY'
+};
